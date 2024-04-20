9537d4dc26b2e93e03061353a80a656b90924b19
Michal <ciurej.michal01@gmail.com>
2024-04-20 11:02:05 +0200

    Additional case

---
+++ JSON_INPUT_ERROR.js
@@ -0,0 +1,19 @@
+const JSON_INPUT_ERROR = {
+    "PolicyName": "root",
+    "PolicyDocument": {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Sid": "IamListAccess",
+                "Effect": "Allow",
+                "Action": [
+                    "iam:ListRoles",
+                    "iam:ListUsers"
+                ],
+                "Resource": " "
+            }
+        ]
+    }
+};
+
+module.exports = JSON_INPUT_ERROR
--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const JSON_INPUT = require('./JSON_INPUT.js')
+const JSON_INPUT_ERROR = require('./JSON_INPUT_ERROR.js')
 const Error_Input_1 = {};
 const Error_Input_2 = 2;
 
@@ -15,5 +16,6 @@ function check(input){
 }
 
 console.log(check(JSON_INPUT));
+console.log(check(JSON_INPUT_ERROR));
 console.log(check(Error_Input_1));
 console.log(check(Error_Input_2));
